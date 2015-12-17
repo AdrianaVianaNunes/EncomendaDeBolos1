191cba28ec73aa92a578d1ef03100f553a137c2d
Adriana Viana Nunes <drikka006@gmail.com>
2015-12-16 22:02:30 -0300

    Configuração do JPA
    Configuração do JPA

---
+++ src/br/com/encomendaDeBolos/teste/Main.java
@@ -0,0 +1,26 @@
+package br.com.encomendaDeBolos.teste;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Pessoa p = new Pessoa();
+		p.setNome("teste");
+		p.setSenha("senha");
+		
+		EntityManagerFactory em = Persistence.createEntityManagerFactory("pessoas-mysql");
+		EntityManager mane = em.createEntityManager();
+		
+		mane.getTransaction().begin();
+		mane.persist(p);
+		p.setNome("Jose");
+		
+		mane.getTransaction().commit();
+		mane.close();
+
+	}
+
+}
